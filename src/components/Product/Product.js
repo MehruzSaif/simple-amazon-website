a82607358a117ee09138d11a3991f82f0e5800e1
MehruzSaif <mehruzsaif007@gmail.com>
2022-03-23 20:39:57 +0600

    added handleEventListener

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,12 +2,21 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-
-    console.log(props.product);
+    // console.log(props.product);
+    const { name, img, seller, price, ratings } = props.product;
 
     return (
-        <div>
-            <h2>This is Product</h2>
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Ratings: {ratings} stars</small></p>
+            </div>
+            <button onClick={() => props.handleAddToCart(props.product)} className='btn-cart'>
+                <p>Add to cart</p>
+            </button>
         </div>
     );
 };
